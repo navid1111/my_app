c6423a32b2972f11b6834e8f827e7634fe96f166
navid-kamal <navidkamal568@gmail.com>
2025-06-23 12:47:59 +0600

    Refactor document loading and processing to include vector store conversion

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+load_dotenv()
 loader = WebBaseLoader(
     "https://medium.com/@navidkamal/how-to-use-mongodb-with-typescript-and-what-might-break-957c1bf17755",
     header_template={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"},
@@ -8,13 +12,19 @@ loader = WebBaseLoader(
 # Then we load it into our document loader
 
 docs = loader.load()
-print(docs)
+# print(docs)
 
 #  we can't give this directly to our llm models because every model has a limit on how many tokens it can process.
 # So we need to split it into smaller chunks.
 
-text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
 
 documents=text_splitter.split_documents(docs)
 
-print(documents)
+# print(documents)
+
+emeddings = OpenAIEmbeddings()
+
+# now we have to convert these documents into vectors
+vectorestore_db= FAISS.from_documents(documents,emeddings)
+print(vectorestore_db)

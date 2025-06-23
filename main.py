bb590a83bfa04a2cfb7e20ba18c204e0c0e7c120
navid-kamal <navidkamal568@gmail.com>
2025-06-23 12:37:42 +0600

    Enhance document processing by adding text splitting functionality

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from langchain_community.document_loaders import WebBaseLoader
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 loader = WebBaseLoader(
     "https://medium.com/@navidkamal/how-to-use-mongodb-with-typescript-and-what-might-break-957c1bf17755",
     header_template={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"},
 )
+# First we read our data from website
+# Then we load it into our document loader
+
 docs = loader.load()
 print(docs)
+
+#  we can't give this directly to our llm models because every model has a limit on how many tokens it can process.
+# So we need to split it into smaller chunks.
+
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+documents=text_splitter.split_documents(docs)
+
+print(documents)
